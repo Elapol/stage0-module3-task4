8af8dfd25f66f952fe722e94e7c9cae753c720b7
elyad <ella.dikovich@gmail.com>
2022-10-25 22:13:20 +0200

    katia2

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,7 +4,10 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a=7;
         int b=2;
-        double result = (9*(a*a) - 5*b + 2 + a - 7)*((a+b-4*a*b)/2);
+        int firstMnozitel= 9*(a*a) - 5*b + 2 + a - 7;
+        int secondMnozitel = a+b-4*a*b;
+        double finalSecondMnozitel = secondMnozitel/2;
+        double result = firstMnozitel*finalSecondMnozitel;
 
         System.out.println(result);
     }
