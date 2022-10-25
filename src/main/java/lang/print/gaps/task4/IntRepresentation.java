cf3ad5d931e48110890e6f9689a9a30f48610235
elyad <ella.dikovich@gmail.com>
2022-10-25 20:02:34 +0200

    ela

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c0 = 'c';
+        int a0 = c0;
+        char c1 = 'h';
+        int a1 = c1;
+        char c2 = 'a';
+        int a2 = c2;
+        char c3 = 'r';
+        int a3 = c3;
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-//         first = 10_000_000_000L;
-//         second = 'a';
-//         third = 5 > 9;
-//         forth = 12.90434323324342;
-//         fifth = 12.3F;
-//         sixth = 1;
-//         seventh = 129;
+        long l1 = 10_000_000_000L;
+        char c = 'a';
+        boolean a = 5 > 9;
+        double d = 12.90434323324342;
+        float f = 12.3F;
+        byte b = 1;
+        short s = 129;
     }
 }
