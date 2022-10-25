872432a5fc4670dc19edda05ee9d9475352590f6
elyad <ella.dikovich@gmail.com>
2022-10-25 21:55:38 +0200

    Sasha

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -5,7 +5,7 @@ public class FormulaCreator {
         int a=7;
         int b=2;
 
-        System.out.println((9*a^2 - 5*b + 2 + a - 7)*((a+b-4*a*b)/2));
+        System.out.println((9*(a*a) - 5*b + 2 + a - 7)*((a+b-4*a*b)/2));
     }
 
 }
